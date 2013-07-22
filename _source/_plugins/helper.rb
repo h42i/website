c1575444f546b74122c4cfb360eb5d6f501f0ded
hasi <hello@hasi.it>
2013-07-22 12:09:35 +0200

    fixed podcast oath

--- _source/_plugins/helper.rb
+++ _source/_plugins/helper.rb
@@ -61,11 +61,10 @@ module Jekyll
     # which lives in /episodes.
     #
     #   {{ "example.m4a" | file_size }} => 4242
-    def file_size(path)
-      return 0 if path.nil?
+    def file_size(file)
+      return 0 if file.nil?
       if File.directory? '../files/podcast'
-        path = path =~ /\// ? path : File.join('../files/podcast', path)
-        path = rel + path if rel
+        path = File.join('../files/podcast', file)
         File.size(path)
       else
         1337
@@ -77,41 +76,6 @@ module Jekyll
     #   {{ page | slug }} => '2012_10_02_octopod'
     def slug(page)
       page['id'][1..-1].gsub('/', '_')
-    end
-
-    # Splits a chapter, like it is written to the post YAML front matter into
-    # the components 'start' which refers to a single point in time relative to
-    # the beginning of the media file nad 'title' which defines the text to be
-    # the title of the chapter.
-    #
-    #   {{ '00:00:00.000 Welcome to Octopod!' | split_chapter }}
-    #     => { 'start' => '00:00:00.000', 'title' => 'Welcome to Octopod!' }
-    #
-    #   {{ '00:00:00.000 Welcome to Octopod!' | split_chapter:'title' }}
-    #     => 'Welcome to Octopod!'
-    #
-    #   {{ '00:00:00.000 Welcome to Octopod!' | split_chapter:'start' }}
-    #     => '00:00:00.000'
-    def split_chapter(chapter_str, attribute = nil)
-      attributes = chapter_str.split(/ /, 2)
-      return nil unless attributes.first.match(/\A(\d|:|\.)+\z/)
-
-      if attribute.nil?
-        { 'start' => attributes.first, 'title' => attributes.last }
-      else
-        attribute == 'start' ? attributes.first : attributes.last
-      end
-    end
-
-    # Returns an <audio>-tag for a given page with <source>-tags in it for every
-    # audio file in the page's YAML front matter.
-    #
-    #   {{ page | audio_tag:site }}
-    def audio_tag(page, site)
-      out = %Q{<audio id="#{slug(page)}_player" preload="none">\n}
-      out + page['audio'].map { |format, filename|
-        %Q{<source src="#{site['url']}/episodes/#{ERB::Util.url_encode(filename)}" type="#{mime_type(format)}"></source>}
-      }.join("\n") + "\n</audio>\n"
     end
 
     # Returns the web player for the episode of a given page.
