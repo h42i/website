30cbe768ea8f0b85972d4c01638d4cf5b10a0939
Leon Shure <hello@l3kn.de>
2013-08-02 13:07:21 +0000

    added env config

--- _source/_plugins/env_config.rb
+++ _source/_plugins/env_config.rb
@@ -1,13 +1,24 @@
 module Jekyll
 	class EnvironmentConfigGenerator < Generator
+		# Default environment configuration
+		DEFAULTS = {
+			'environment' => 'development'
+		}
+		
 		def generate(site)
 			file = File.join(Dir.pwd, "_env.yml")
-    	env_config = YAML.safe_load_file(file)
+			site.config['environment'] = read_config_file(file)['environment']
+      Jekyll.logger.info "Environment:", site.config['environment']
+		end
+		
+		def read_config_file(file)
+      env_config = YAML.safe_load_file(file)
       raise ArgumentError.new("Environment Configuration file: (INVALID) #{file}".yellow) if !env_config.is_a?(Hash)
-      Jekyll.logger.info "Environment Configuration file:", file
-			site.config['env'] = env_config
-		rescue SystemCallError
-			Jekyll.logger.warn "Environment Configuration file:", "none"
+      Jekyll.logger.info "Environment file:", file
+      env_config
+    rescue SystemCallError
+      Jekyll.logger.warn "Environment file:", "none"
+			DEFAULTS	
 		end
 	end
 end
