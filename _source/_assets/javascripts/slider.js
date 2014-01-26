f7348667fcc36f8ac6d7d34f21cf05902f01b078
Leon Rische <hello@l3kn.de>
2014-01-26 21:22:56 +0100

    fancyer slider

--- _source/_assets/javascripts/slider.js
+++ _source/_assets/javascripts/slider.js
@@ -2,11 +2,21 @@ window.onload = function() {
   var index = 0;
   var slider = document.getElementById('slider');
   var images = slider.getElementsByTagName('img');
+  var progress = slider.getElementsByClassName('progress')[0];
+  
+  var time_last  = new Date().getTime();
+  var delay = 5000;
   
   setInterval(function() {
-    index = (index+1) % images.length;
-    for (var i = 0; i < images.length; ++i) {
-      images[i].style.opacity= ((i == index) ? 1 : 0);
+    var time_new = new Date().getTime();
+    var time_diff = time_new - time_last;
+    if (time_diff >= delay) {
+      time_last = time_new;
+      index = (index+1) % images.length;
+      for (var i = 0; i < images.length; ++i) {
+        images[i].style.opacity= ((i == index) ? 1 : 0);
+      }
     }
-  }, 5000)
+    progress.style.width = (time_diff/delay*100 + '%')
+  }, 10)
 };
