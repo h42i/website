628d2442ee1e0096a0731a99c830f926b0288251
Leon Rische <hello@l3kn.de>
2014-01-26 21:06:34 +0100

    fancyer slider

--- _source/_assets/javascripts/slider.js
+++ _source/_assets/javascripts/slider.js
@@ -1,14 +1,12 @@
 window.onload = function() {
   var index = 0;
   var slider = document.getElementById('slider');
-  var images = slider.getAttribute('data-images').split(',');
-  console.log(images);
-  slider.style.backgroundImage="url(\'" + images[0] + "\')"
+  var images = slider.getElementsByTagName('img');
   
   setInterval(function() {
     index = (index+1) % images.length;
-    console.log(index);
-    slider.style.backgroundImage="url(\'" + images[index] + "\')"
-    console.log("tick");
+    for (var i = 0; i < images.length; ++i) {
+      images[i].style.opacity= ((i == index) ? 1 : 0);
+    }
   }, 5000)
 };
