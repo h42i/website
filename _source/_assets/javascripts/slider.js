4b66820666881af2abc48e63b0d293e06b6ff393
Leon Shure <hello@l3kn.de>
2014-01-28 20:43:52 +0000

    fixed slider

--- _source/_assets/javascripts/slider.js
+++ _source/_assets/javascripts/slider.js
@@ -1,22 +1,36 @@
 window.onload = function() {
   var slider = document.getElementById('slider');
-  var images = slider.getElementsByTagName('img');
-  var progress = slider.getElementsByClassName('progress')[0];
-  var delay = 5000;
-  var time_last  = new Date().getTime();
-  var index = images.length - 1;
-  
-  setInterval(function() {
-    var time_new = new Date().getTime();
-    var time_diff = time_new - time_last;
-    if (time_diff >= delay) {
-      time_last = time_new;
-      index = (index+1) % images.length;
-      console.log(index);
-      for (var i = 0; i < images.length; i++) {
-        images[i].style.opacity= ((i == index) ? 1 : 0);
-      }
+  var sources = ["/images/slider/slide1.jpg"
+                ,"/images/slider/slide2.jpg"
+                ,"/images/slider/slide3.jpg"
+                ,"/images/slider/slide4.jpg"
+                ,"/images/slider/slide5.jpg"
+                ,"/images/slider/slide6.jpg"];
+
+  var images = [];
+  var index = 1;
+
+  var display = window.getComputedStyle(slider)['display'];
+  if (display !== 'none') {
+    for (var i = 0; i < sources.length; i++) {
+      images[i] = new Image;
+      images[i].src = sources[i];
+      console.log(sources[i]);
     }
-    progress.style.width = (time_diff/delay*100 + '%')
-  }, 10)
+
+    var progress = slider.getElementsByClassName('progress')[0];
+    var delay = 5000;
+    var time_last = new Date().getTime();
+     
+    setInterval(function() {
+      var time_new = new Date().getTime();
+      var time_diff = time_new - time_last;
+      if (time_diff >= delay) {
+        time_last = time_new;
+        index = (index+1) % images.length;
+        slider.style.backgroundImage = ("url(" + sources[index] + ")");
+      }
+      progress.style.width = (time_diff/delay*100 + '%')
+    }, 10)
+  }
 };
